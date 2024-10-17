f69fd8a76eb57a06e083937094420af41edd7ad7
Giovanni Almeida Argento <giovanni@ele.puc-rio.br>
2024-10-17 11:55:24 -0300

    add newsvendor tests

---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+using ApplicationDrivenLearning
+using Flux
+using JuMP
+using HiGHS
+using Optim
+using BilevelJuMP
+using Test
+
+include("utils.jl")
+include("test_newsvendor.jl")
---
+++ test/test_newsvendor.jl
@@ -0,0 +1,74 @@
+c = 5.0
+q = 9.0
+r = 4.0
+X = ones(1, 1)
+Y = 50 * ones(1, 1)
+best_decision = y = Y[1, 1]
+best_cost = (c-q)*y
+
+model = ApplicationDrivenLearning.Model()
+@variables(model, begin
+    x, ApplicationDrivenLearning.Policy
+    d, ApplicationDrivenLearning.Forecast
+end)
+@variables(ApplicationDrivenLearning.Plan(model), begin
+    y >= 0
+    w >= 0
+end)
+@constraints(ApplicationDrivenLearning.Plan(model), begin
+    con1, y <= d.plan
+    con2, y + w <= x.plan
+end)
+@objective(ApplicationDrivenLearning.Plan(model), Min, c*x.plan-q*y-r*w)
+@variables(ApplicationDrivenLearning.Assess(model), begin
+    y >= 0
+    w >= 0
+end)
+@constraints(ApplicationDrivenLearning.Assess(model), begin
+    con1, y <= d.assess
+    con2, y + w <= x.assess
+end)
+@objective(ApplicationDrivenLearning.Assess(model), Min, c*x.assess-q*y-r*w)
+set_optimizer(model, HiGHS.Optimizer)
+set_silent(model)
+nn = Chain(
+    Dense(1 => 1; bias=false),
+)
+
+@testset "Newsvendor BilevelMode" begin
+    ApplicationDrivenLearning.set_forecast_model(model, nn)
+    opt = ApplicationDrivenLearning.Options(
+        ApplicationDrivenLearning.BilevelMode, 
+        optimizer=HiGHS.Optimizer,
+        mode=BilevelJuMP.FortunyAmatMcCarlMode(primal_big_M=100, dual_big_M=100)
+    )
+    sol = ApplicationDrivenLearning.train!(model, X, Y, opt)
+    @test sol.params[1] ≈ best_decision atol=1e-2
+    @test sol.cost ≈ best_cost atol=1e-2
+end
+
+@testset "Newsvendor NelderMeadMode" begin
+    ApplicationDrivenLearning.set_forecast_model(model, nn)
+    opt = ApplicationDrivenLearning.Options(
+        ApplicationDrivenLearning.NelderMeadMode, 
+        iterations=100,
+        time_limit=60
+    )
+    sol = ApplicationDrivenLearning.train!(model, X, Y, opt)
+    @test sol.params[1] ≈ best_decision atol=1e-2
+    @test sol.cost ≈ best_cost atol=1e-2
+end
+
+@testset "Newsvendor GradientMode" begin
+    ApplicationDrivenLearning.set_forecast_model(model, nn)
+    opt = ApplicationDrivenLearning.Options(
+        ApplicationDrivenLearning.GradientMode;
+        rule=Flux.Adam(1.0), 
+        epochs=300,
+        batch_size=-1, 
+        verbose=true
+    )
+    sol = ApplicationDrivenLearning.train!(model, X, Y, opt)
+    @test sol.params[1] ≈ best_decision atol=1e-2
+    @test sol.cost ≈ best_cost atol=1e-2
+end
