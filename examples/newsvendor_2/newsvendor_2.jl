bfb644db81aa50a7722006b430072db6f2ecb03d
Giovanni3A <giovanni3amorim@gmail.com>
2025-05-28 13:45:17 +0000

    run bilevel mode experiment with one predictive model for each newsvendor item

--- examples/newsvendor_2/newsvendor_2.jl
+++ examples/newsvendor_2/newsvendor_2.jl
@@ -30,10 +30,12 @@ end
 # generate data X,Y
 Y = Matrix{Float32}(undef, T+p, I)
 Y[1:p, :] .= rand(Distributions.Normal(μ, σ), (p, I))
-α = rand(Distributions.Uniform(0.1, 1.0), p)
-α = 1.0 * α ./ sum(α)  # Normalize to sum to 1
+α = rand(Distributions.Uniform(0.1, 1.0), (p, I))
+α = α ./ sum(α, dims=1)  # Normalize to sum to 1
 for t in (p+1):T+p
-    Y[t, :] = (α'Y[t-p:t-1, :])' .+ rand(Distributions.Normal(0.0, σ), I)
+    for i=1:I
+        Y[t, i] = (α[:, i]'Y[t-p:t-1, i])' .+ rand(Distributions.Normal(0.0, σ))
+    end
 end
 X = Matrix{Float32}(undef, T, I*p)
 for t=1:T
@@ -45,17 +47,22 @@ for t=1:T
 end
 Y = Y[p+1:end, :]
 
-# compute least-squares model
-xs = vcat([hcat(X[:, (i-1)*p+1:i*p], ones(T)) for i=1:I]...) .|> Float64
-ys = vcat([Y[:, i] for i=1:I]...) .|> Float64
-linear_ls_model = GLM.lm(xs, ys)
-θ = Float32.(GLM.coef(linear_ls_model))
+# compute least-squares models (one for each item)
+θ = zeros((p+1, I))
+ls_models = []
+for i=1:I
+    xs = hcat(X[:, p*(i-1)+1:p*i], ones(T)) .|> Float64
+    ys = Y[:, i] .|> Float64
+    ls_model = GLM.lm(xs, ys)
+    push!(ls_models, ls_model)
+    θ[:, i] .= Float32.(GLM.coef(ls_model))
+end
 
 # build application driven learning model
 model = ApplicationDrivenLearning.Model()
 @variables(model, begin
     x[1:I] ≥ 0, ApplicationDrivenLearning.Policy
-    d[1:I] ≥ 0, ApplicationDrivenLearning.Forecast
+    d[1:I], ApplicationDrivenLearning.Forecast
 end)
 function build_nesvendor_jump_model(jump_model, x, d)
     @variables(jump_model, begin
@@ -75,18 +82,28 @@ set_optimizer(model, Gurobi.Optimizer)
 set_silent(model)
 
 # declare predictive model
-nn = Flux.Chain(
-    Flux.Dense(p => 1)
-)
-Flux.params(nn)[1][1, :] .= θ[1:end-1]
-Flux.params(nn)[2][1] = θ[end]
-input_output_map = Dict(
-    collect((i-1)*p+1:i*p) => [i]
+nns = [
+    Flux.Chain(Flux.Dense(p => 1))
     for i=1:I
-)
+]
+for i=1:I
+    Flux.params(nns[i])[1][1, :] .= θ[1:end-1, i]
+    Flux.params(nns[i])[2][1] = θ[end, i]
+end
+input_output_map = [
+    Dict(
+        collect((i-1)*p+1:i*p) => [i]
+    )
+    for i=1:I
+]
 ApplicationDrivenLearning.set_forecast_model(
     model,
-    ApplicationDrivenLearning.PredictiveModel(deepcopy(nn), input_output_map)
+    ApplicationDrivenLearning.PredictiveModel(
+        deepcopy(nns), 
+        input_output_map, 
+        size(X, 2),
+        size(Y, 2)
+    )
 )
 
 # least-squares model prediction and cost
@@ -106,14 +123,20 @@ yhat_opt = model.forecast(X')'
 cost_opt = ApplicationDrivenLearning.compute_cost(model, X, Y)
 
 println("Cost LS: $cost_ls | Cost OPT: $cost_opt")
+println("Improvement: $(100*(cost_opt-cost_ls)/ cost_ls)%")
 
 # uncertainty analysis
-bias1 = abs.(yhat_opt[:, 1] .- yhat_ls[:, 1])
-bias2 = abs.(yhat_opt[:, 2] .- yhat_ls[:, 2])
-prediction, lower, upper = StatsAPI.predict(linear_ls_model, xs; interval=:confidence, level=0.95)
-uncertainty = reshape((upper - lower) ./ 2, (T, I))
-corr1 = Statistics.cor(uncertainty[:, 1], bias1)
-corr2 = Statistics.cor(uncertainty[:, 2], bias2)
+bias = Matrix{Float64}(undef, T, I)
+for i=1:I
+    bias[:, i] .= abs.(yhat_opt[:, i] .- yhat_ls[:, i])
+end
+uncertainty = Matrix{Float64}(undef, T, I)
+for i=1:I
+    xs = hcat(X[:, p*(i-1)+1:p*i], ones(T)) .|> Float64
+    prediction, lower, upper = StatsAPI.predict(ls_models[i], xs; interval=:confidence, level=0.95)
+    uncertainty[:, i] .= upper - lower
+end
+corr = [Statistics.cor(uncertainty[:, i], bias[:, i]) for i=1:I]
 
 # compare predictions
 MAX_TIMESTEPS_PLOT = 100
@@ -130,7 +153,9 @@ savefig(fig2, joinpath(IMGS_PATH, "newsvendor_2_demand.png"))
 # compare errors
 err_ls = (yhat_ls .- Y)
 err_opt = (yhat_opt .- Y)
-bins = -10:1:10
+min_bin = minimum([minimum(err_ls), minimum(err_opt)])
+max_bin = maximum([maximum(err_ls), maximum(err_opt)])
+bins = min_bin:1:max_bin
 fig3 = histogram(
     err_ls[:, 1], 
     alpha=.7,
@@ -164,19 +189,17 @@ histogram!(
 savefig(fig4, joinpath(IMGS_PATH, "newsvendor_2_errors.png"))
 
 # scatter plot
-fig4 = scatter(
-    bias1,
-    uncertainty[:, 1],
-    alpha=.7,
+fig5 = plot(
     title="Relationship between uncertainty and bias",
-    label="Newsvendor 1 (corr=$(round(100*corr1, digits=1))%)",
     xlabel="Bias",
     ylabel="Uncertainty"
 )
-scatter!(
-    bias2,
-    uncertainty[:, 2],
-    alpha=.7,
-    label="Newsvendor 2 (corr=$(round(100*corr2, digits=1))%)"
-)
+for i=1:I
+    scatter!(
+        bias[:, i],
+        uncertainty[:, i],
+        alpha=.7,
+        label="Newsvendor $i (corr=$(round(100*corr[i], digits=1))%)",
+    )
+end
 savefig(joinpath(IMGS_PATH, "uncertainty_bias.png"))
